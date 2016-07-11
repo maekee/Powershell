9b713b2c2fce104eb47893c05080a3869c68a577
Maekee <drmicke@gmail.com>
2016-07-11 13:48:45 +0200

    Create Get-GUIValue.ps1

---
+++ NiceToHave/Get-GUIValue.ps1
@@ -0,0 +1,17 @@
+Function Get-GUIValue{
+    [CmdletBinding()]
+    param([string]$Title,[string]$Description)
+
+    IF($PSVersionTable.PSVersion.Major -lt 2){ Write-Warning "Powershell v1 not supported";Break }
+    IF(-NOT $Title){ $TitleValue = "Enter value in the textbox" }ELSE{$TitleValue = $Title}
+    IF(-NOT $Description){$DescriptionValue = "Please enter the desired value in the textbox"}ELSE{$DescriptionValue = $Description}
+
+    TRY{
+        Add-Type -AssemblyName Microsoft.VisualBasic
+        $ReturnedValue = $( [Microsoft.VisualBasic.Interaction]::InputBox($DescriptionValue,$TitleValue, "<enter here>") )
+    }
+    CATCH {Break}
+
+    IF($ReturnedValue -eq '<enter here>'){$ReturnedValue = $null}
+    return $ReturnedValue
+}
