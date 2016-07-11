5c104d071f52f89d25f3e9ac48914cbcf0872c02
Maekee <drmicke@gmail.com>
2016-07-11 16:16:08 +0200

    Create Get-PrimaryDevice.ps1

---
+++ SCCM/Get-PrimaryDevice.ps1
@@ -0,0 +1,20 @@
+## Simple Function go get primary device from SCCM via WMI
+
+Function Get-PrimaryDevice {
+    [CmdletBinding()]
+    param (
+        [string]$Username,
+        [string]$SCCMSiteCode,
+        [string]$SCCMSiteServer)
+
+    TRY{$DataReturnedFromWMI = Get-WmiObject -Namespace "Root\SMS\Site_$SCCMSiteCode" -Class "SMS_UserMachineRelationship" -Filter "(UniqueUserName='ppm\\$Username')" -ComputerName $SCCMSiteServer -ErrorAction Stop }
+    CATCH{ Write-Warning "Problem occurred while getting data from WMI. Exception: $($_.Exception.Message)" }
+
+    $ReturnFromFunction = @($DataReturnedFromWMI | Where {$_.ResourceClientType -eq "1" -AND $_.IsActive -eq $true})
+    IF($ReturnFromFunction.Count -eq 0){ Write-Verbose "No Primary devices detected";return $null }
+    ELSE{
+        $ReturnFromFunction | Select @{Name="PrimaryDevices";Expression={($_.ResourceName).ToUpper()}},@{Name="TimeStamp";Expression={ Get-Date ([Management.ManagementDateTimeConverter]::ToDateTime( $_.CreationTime )) -format "yyyy-MM-dd HH:mm" } }
+    }
+}
+
+#Usage example: Get-PrimaryDevice -Username <username> -SCCMSiteCode CM1 -SCCMSiteServer SCCMServer01
