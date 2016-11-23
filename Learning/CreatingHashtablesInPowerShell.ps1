4878303f3f11ebd3e83369ca53dcf3d746acf4cc
Maekee <drmicke@gmail.com>
2016-11-23 19:52:08 +0100

    Create CreatingHashtablesInPowerShell.ps1

---
+++ Learning/CreatingHashtablesInPowerShell.ps1
@@ -0,0 +1,50 @@
+## Hashtables is really useful in PowerShell. Once you start using them, you wont stop :)
+## A hashtable is a data structure like an array, but hashtables store their values in key/value pairs.
+
+## Lets create a hashtable:
+$myFirstHash = @{}
+
+## They are defined almost the same as an array that uses parenthesis e.g. @classicArray = @()
+## You can add data to the hashtable like this...
+
+$myFirstHash.Add("Michael","Stockholm")
+
+## or with variables
+$key = "Torgny"
+$value = "Malmö"
+$myFirstHash.Add($key,$value)
+
+## You can also create the hashtable with pre-populated keys and values..
+$myFirstHash = @{
+  Michael = "Stockholm"
+  Torgny = "Malmö"
+}
+
+## Or why not a one-liner with semicolons
+$myFirstHash = @{ Michael = "Stockholm" ; Torgny = "Malmö" }
+
+## Once data is in there, you probably want to go get the data. Can be done in more than one way.
+## Using the Key to get the value back:
+$myFirstHash['Torgny']
+
+## Very easy to update Torgnys home town:
+$myFirstHash['Torgny'] = "Sollefteå"
+
+### Hashtables are really useful to use to dynamically populate and get data.
+## for example inserting all AD-users in a specific OU:
+$myADUserHash = @{}
+Get-ADUser -filter * -Searchbase "OU=blabla,OU=blabla,DC=bla,DC=bla" | foreach {$myADUserHash.Add($_.SamAccountName,$_)}
+
+## Now you can really easy get my DN by pointing to my key in the hashtable which is my SamAccountName (username).
+$myADUserHash.maekee.DistinguishedName
+
+## This works because on the value i placed the AD user object instead of a string value.
+### Hashtables crush arrays in speed when you come up to a few hundred or thousand entrys.
+
+### One thing that hashtables cannot easily as arrays is iterating through them.
+## The pipeline treats the hashtable as one object ("$myHash | Measure-Object" returns 1)
+## What you need to do is iterate through either only the Key or the Value like this:
+$myHash.keys | foreach {"do this"}
+
+## If you want both keys and values you need to use the method GetEnumerator(), like this:
+...
