6eb5f14e98aae81c51c865eed7605c8f3208695c
Maekee <MickesMacBookPro@Mickes-MacBook-Pro.local>
2016-11-23 21:02:52 +0100

    Here comes more info on hashtables

--- Learning/CreatingHashtablesInPowerShell.ps1
+++ Learning/CreatingHashtablesInPowerShell.ps1
@@ -28,6 +28,9 @@ $myFirstHash = @{}
 $myFirstHash.Micke = "Stockholm"
 $myFirstHash.Torgny = "Malmö"
 
+#The addition operator is also supported:
+$myFirstHash += @{Micke = "Stockholm”}
+
 ## Once data is in there, you probably want to go get the data. Can be done in more than one way.
 ## Using the Key to get the value back:
 $myFirstHash['Torgny']
@@ -44,7 +47,7 @@ Get-ADUser -filter * -Searchbase "OU=blabla,OU=blabla,DC=bla,DC=bla" | foreach {
 $myADUserHash.maekee.DistinguishedName
 
 ## This works because on the value i placed the AD user object instead of a string value.
-### Hashtables crush arrays in speed when you come up to a few hundred or thousand entrys.
+## Hashtables crush arrays in speed when you come up to a few hundred or thousand entrys.
 
 ### One thing that hashtables cannot easily as arrays is iterating through them.
 ## The pipeline treats the hashtable as one object ("$myHash | Measure-Object" returns 1)
@@ -54,6 +57,68 @@ $myHash.keys | foreach {"do this"}
 ## If you want both keys and values you need to use the method GetEnumerator(), like this:
 $myHash.GetEnumerator() | foreach {"$($_.key) is the Key and $($_.value) is the value"}
 
-## So, we have a hashtable with some key/value pairs
-$per @@
-#Hashtable inception? You can place hash tables inside an hashtable!
+### So, we have a hashtable with some key/value pairs
+$personHash = @{}
+$personHash.Name = ”Micke”
+$personHash.Age = 36
+$personHash.Location = ”Sweden”
+
+## Now you can navigate around the values with (like an object):
+$personHash.Name or $personHash.Age
+
+## Hashtable inception!? You can place an new hashtables inside an hashtable!
+$personHash.Location = @{}
+$personHash.Location.Country = ”Sweden
+$personHash.Location.City = ”Stockholm”
+$personHash.Location.AreaCode = ”08”
+
+## or just like this:
+@personHash = @{
+  Name = ”Micke”
+  Age = 36
+  Location = @{
+    Country = ”Sweden”
+    City = ”Stockholm”
+    AreaCode = ”08”
+  }
+}
+
+## And then navigate around like this to the values:
+$personHash.Location.City
+
+## Why not like this:
+$peopleHash = @{
+  Micke = @{
+    Age = 36
+    Location = @{
+      Country = ”Sweden”
+      City = ”Stockholm”
+      AreaCode = ”08”
+    }
+  }
+  Torgny = @{
+    Age = 54
+    Location = @{
+      Country = ”Sweden”
+      City = ”Malmö”
+      AreaCode = ”040”
+    }
+  }
+}
+
+### You get Torgnys City like this:
+$peopleHash.Torgny.City
+
+
+
+… Coming up.. splatting and for optional parameters
+Easy way to show content in hashtable: @hashTable | ConvertTo-Json
+
+Save to file and read from file:
+$hash | ConvertTo-Json | Set-Content -Path $path
+$hash = Get-Content -path $path -Raw | ConvertFrom-Json
+
+Import file with powershell syntax… [scriptblock]…
+
+Creating object from hash table: [pscustomobject]$myHash
+This will give you column names…
