3f1317f3a1e310f033e2232e4e399083b512d3d3
Maekee <drmicke@gmail.com>
2017-04-21 20:39:17 +0200

    Create WebExwithPowerShell.ps1

---
+++ WebEx/WebExwithPowerShell.ps1
@@ -0,0 +1,5 @@
+## This PowerShell code can be updated even more with stuff like comment based help, improved credential handling,
+## separate parameters for the global variables and so on. But i wanted to share this before i move on to the next assignment so
+## everyone that is interested and struggles with this can get some help. Maybe i will come back and fix the missing things.
+## Your feedback and improvements are highly welcome
+## Enjoy //Maekee
