997909f84b68d8c82d4ebbb08a4a7ed9475a6ce9
Maekee <drmicke@gmail.com>
2017-08-25 08:49:20 +0200

    Update TextFileToArray.ps1
    added the support for values with spaces if double quotation is used

--- NiceToHave/TextFileToArray.ps1
+++ NiceToHave/TextFileToArray.ps1
@@ -2,6 +2,7 @@
 #If needed, just replace the split on the space for what ever you want.
 #Examples of this can be output from commands, Firewall logs etc.
 #Column names, number of columns, values and number of rows can be changed.
+#The black regex magic is to support column names and values with spaces if double quotation marks is used (i.e "").
 
 #Example:
 #column1 column2 column3
@@ -10,12 +11,12 @@
 #here there everywhere
 
 $FileContent = Get-Content C:\LogFiles\FWLogfile.txt -Encoding UTF8
-$PropertyNames = $FileContent[0] -split ' '
+$PropertyNames = $FileContent[0] -split  ' +(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)'
 $Array = New-Object System.Collections.ArrayList
 
 #Create PSObjects and add to Arraylist $Array
 foreach ($FileRow in ($FileContent | Select -Skip 1) ){
-    $FileRowSplit = $FileRow -split ' '
+    $FileRowSplit = $FileRow -split  ' +(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)'
     $propHash = [Ordered]@{}
 
     for ($i = 0; $i -lt $PropertyNames.Count; $i++){ 
