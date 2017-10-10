799a47d9645e750c9e21b4bf4fecb6e1a61cac3d
Maekee <drmicke@gmail.com>
2017-10-10 11:04:26 +0200

    Create Get-ESClusterNodes.ps1

---
+++ ElasticSearch/Get-ESClusterNodes.ps1
@@ -0,0 +1,20 @@
+ Function Get-ESClusterNodes{
+    [CmdletBinding()]
+    param(
+        $ElasticSearchUri = 'http://localhost:9200',
+        $NodeName
+    )
+
+    $JsonData = Invoke-WebRequest -Uri "$ElasticSearchUri/_nodes/_all" -Method Get -ContentType application/json
+    $JsonObj = ConvertFrom-Json -InputObject $JsonData.Content
+    
+    if( @($JsonObj.nodes).count -ne 0 ){
+        $NodeIdList =  @($JsonObj.nodes | Get-Member | Where {$_.Membertype -eq "NoteProperty"}).Name
+        $ObjList = @()
+        $ObjList = $NodeIdList | foreach { $JsonObj.nodes.$($_) }
+    }
+    else{ Write-Verbose "No elasticsearch nodes found" }
+
+    if($NodeName){ $ObjList | Where {$_.Name -eq $NodeName} }
+    else{$ObjList}
+}
