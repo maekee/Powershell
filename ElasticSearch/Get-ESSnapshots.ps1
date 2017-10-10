152e63319de598ee5b3e71a566bd2064f539fea2
Maekee <drmicke@gmail.com>
2017-10-10 11:09:14 +0200

    Create Get-ESSnapshots.ps1

---
+++ ElasticSearch/Get-ESSnapshots.ps1
@@ -0,0 +1,12 @@
+ Function Get-ESSnapshots{
+    [CmdletBinding()]
+    param(
+        $ElasticSearchUri = 'http://localhost:9200',
+        [Parameter(Mandatory=$true)]$SnapshotRepository
+    )
+
+    if((Get-ElasticSearchSnapshotRepository).Name -contains $SnapshotRepository){
+        (ConvertFrom-Json -InputObject ( Invoke-WebRequest -Uri "$ElasticSearchUri/_snapshot/$SnapshotRepository/_all" -Method Get ).Content).snapshots
+    }
+    else{ Write-Warning "ElasticSearch snapshot repository $SnapshotRepository not found" }
+}
