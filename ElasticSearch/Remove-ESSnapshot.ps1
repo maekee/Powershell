ff8b3234e3d287c64e894d70e6e520ba921a0fab
Maekee <drmicke@gmail.com>
2017-10-10 11:09:51 +0200

    Create Remove-ESSnapshot.ps1

---
+++ ElasticSearch/Remove-ESSnapshot.ps1
@@ -0,0 +1,28 @@
+ Function Remove-ESSnapshot {
+    [CmdletBinding()]
+    param (
+        $ElasticSearchUri = 'http://localhost:9200',
+        [Parameter(Mandatory=$true)]$SnapshotRepository,
+        [Parameter(Mandatory=$true)]$SnapshotName
+    )
+
+    $SnapshotRepos = @(Get-ElasticSearchSnapshotRepository)
+    if($SnapshotRepos.Count -ne 0 -and $SnapshotRepos.Name -contains $SnapshotRepository){
+        
+        $RepoSnapshots = @(Get-ElasticSearchSnapshots -SnapshotRepository $SnapshotRepository)
+        if($RepoSnapshots.snapshot -contains $SnapshotName){
+           try{
+                Write-Verbose "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") - Removing snapshot $SnapshotName from repository $SnapshotRepository..."
+                Write-Verbose "Removing all files that are associated with snapshot $SnapshotName and not used by any other snapshots."
+                
+                try{ $RemovalResults = Invoke-WebRequest -Uri "$ElasticSearchUri/_snapshot/$SnapshotRepository/${SnapshotName}" -Method Delete -ErrorAction Stop }
+                catch{ Write-Warning $_.Exception.Message }
+                
+                Write-Verbose "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") - Snapshot removal status response: $($RemovalResults.Content)"
+           }
+           catch{ Write-Host $_.Exception.Message }
+        }
+        else{ Write-Warning "Snapshot $SnapshotName could not be found in repository $SnapshotRepository" }
+    }
+    else{ Write-Warning "Snapshot repository $SnapshotRepository not found, specify correct snapshot repository" }
+}
