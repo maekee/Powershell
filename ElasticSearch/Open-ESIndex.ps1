b73cabb414f2401f2437af32d3ea60e75d270070
Maekee <drmicke@gmail.com>
2017-10-10 11:07:39 +0200

    Create Open-ESIndex.ps1

---
+++ ElasticSearch/Open-ESIndex.ps1
@@ -0,0 +1,20 @@
+ Function Open-ESIndex {
+    [CmdletBinding()]
+    param (
+        $ElasticSearchUri = 'http://localhost:9200',
+        [Parameter(Mandatory=$true)]$IndexName,
+        [switch]$Force
+    )
+
+    $CurrentIndex = Get-ElasticSearchIndex -IndexName $IndexName
+    if($CurrentIndex){
+        if($CurrentIndex.status -ne "open" -or $Force){
+            Write-Verbose "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") - Opening index $IndexName..."
+            $closeOutput = Invoke-WebRequest -Method Post -Uri "$ElasticSearchUri/$IndexName/_open" -ContentType application/json
+            Write-Verbose "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") - Open request complete with status: $($closeOutput.content)"
+        }
+        else{ Write-Warning "$IndexName is already in status open, use the force parameter if you still want to request the open index operation" }
+
+    }
+    else{ Write-Warning "Elasticsearch index $IndexName not found" }
+}
