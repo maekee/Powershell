02311caca07bd027a0db15c12ea5753e86f1eee9
Maekee <drmicke@gmail.com>
2017-10-10 11:05:13 +0200

    Create Get-ESShards.ps1

---
+++ ElasticSearch/Get-ESShards.ps1
@@ -0,0 +1,19 @@
+ Function Get-ESShards {
+    [CmdletBinding()]
+    param (
+        $ElasticSearchUri = 'http://localhost:9200',
+        $Filter,
+        [ValidateSet('AllShards','OnlyPrimaryShards','OnlyReplicaShards')]$ShardTypes = 'AllShards',
+        [switch]$IncludeKibanaIndex
+    )
+
+    $AllShards = ConvertFrom-Json -InputObject (Invoke-WebRequest -Uri "$ElasticSearchUri/_cat/shards?format=json" -Method Get -ContentType "application/json").Content
+
+    #Filtering output
+    if(!($IncludeKibanaIndex)){ $AllShards = $AllShards | Where {$_.index -ne ".kibana" }}
+    if($filter){ $AllShards = $AllShards | Where {$_.index -match $Filter} }
+    if($ShardTypes -eq 'OnlyPrimaryShards'){ $AllShards = $AllShards | Where {$_.prirep -eq 'p'} }
+    elseif($ShardTypes -eq 'OnlyReplicaShards'){ $AllShards = $AllShards | Where {$_.prirep -eq 'r'} }
+
+    $AllShards
+}
