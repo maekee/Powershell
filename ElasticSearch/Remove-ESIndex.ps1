13e552e9f772ea9ec6f05b873fc17033d90de421
Maekee <drmicke@gmail.com>
2017-10-10 11:07:56 +0200

    Create Remove-ESIndex.ps1

---
+++ ElasticSearch/Remove-ESIndex.ps1
@@ -0,0 +1,18 @@
+ Function Remove-ESIndex {
+    [CmdletBinding()]
+    param (
+        $ElasticSearchUri = 'http://localhost:9200',
+        [Parameter(Mandatory=$true)]$IndexName
+    )
+
+    try{
+        $CurrentIndex = Get-ElasticSearchIndex -IndexName $IndexName
+        if($CurrentIndex){
+            Write-Verbose "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") - Removing index $IndexName..."
+            $removalOutput = Invoke-WebRequest -Method Delete -Uri "$($ElasticSearchUri)/${IndexName}" -ContentType application/json -ErrorAction Stop
+            Write-Verbose "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") - Removal request complete with status: $($removalOutput.content)"
+        }
+        else{ Write-Warning "Elasticsearch index $IndexName not found" }
+    }
+    catch { Write-Warning "Exception: $((ConvertFrom-Json -InputObject $_.ErrorDetails.Message).error.reason)" }
+}
