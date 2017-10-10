4eb5c1c326186c402500e9d5592dfaf1d82cac52
Maekee <drmicke@gmail.com>
2017-10-10 11:08:21 +0200

    Create Get-ESIndexTemplates.ps1

---
+++ ElasticSearch/Get-ESIndexTemplates.ps1
@@ -0,0 +1,16 @@
+ Function Get-ESIndexTemplates{
+    [CmdletBinding()]
+    param($ElasticSearchUri = 'http://localhost:9200')
+
+    $TemplateData = Invoke-WebRequest -Uri "$ElasticSearchUri/_template" -Method Get -ContentType application/json
+    $TemplateObj = @(ConvertFrom-Json -InputObject $TemplateData.Content)
+
+    if($TemplateObj.Count -ne 0){
+        $TemplateList = @($TemplateObj | Get-Member | Where {$_.Membertype -eq "NoteProperty"}).Name
+        $TemplateList | foreach {     
+            $CurrentObj = (ConvertFrom-Json -InputObject (Invoke-WebRequest -Uri "$ElasticSearchUri/_template" -Method Get -ContentType application/json).Content).$_
+            [PSCustomObject]@{ Name = $_;Order = $CurrentObj.order;Template = $CurrentObj.template;Settings = $CurrentObj.settings;Mappings = $CurrentObj.mappings;Aliases = $CurrentObj.aliases}
+        }
+    }
+    else{ Write-Verbose "No templates found" }
+}
