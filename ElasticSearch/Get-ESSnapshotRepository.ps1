5f2b9f212e7589c02394cd029337e3ee7be8727c
Maekee <drmicke@gmail.com>
2017-10-10 11:08:58 +0200

    Create Get-ESSnapshotRepository.ps1

---
+++ ElasticSearch/Get-ESSnapshotRepository.ps1
@@ -0,0 +1,15 @@
+ Function Get-ESSnapshotRepository {
+    [CmdletBinding()]
+    param($ElasticSearchUri = 'http://localhost:9200')
+    
+    $SnapShots = @((ConvertFrom-Json -InputObject (Invoke-WebRequest -Uri "${ElasticSearchUri}/_snapshot/" -Method Get -ContentType application/json).Content))
+    
+    if($SnapShots.Count -ne 0){
+        $SnapShotList = @($SnapShots | Get-Member | Where {$_.Membertype -eq "NoteProperty"}).Name
+        $SnapShotList | foreach {     
+            $CurrentObj = (ConvertFrom-Json -InputObject (Invoke-WebRequest -Uri "${ElasticSearchUri}/_snapshot/" -Method Get)).$_
+            [PSCustomObject]@{ Name = $_;Type = $CurrentObj.type;Settings = $CurrentObj.settings}
+        }
+    }
+    else{ $null }
+}
