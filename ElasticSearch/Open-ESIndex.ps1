c6ac813fa40f5d443923e25aabb23b98c0f7f9f7
Maekee <drmicke@gmail.com>
2017-11-21 10:55:41 +0100

    Update Open-ESIndex.ps1
    Added the UseBasicParsing parameter to Invoke-WebRequest

--- ElasticSearch/Open-ESIndex.ps1
+++ ElasticSearch/Open-ESIndex.ps1
@@ -10,7 +10,7 @@
     if($CurrentIndex){
         if($CurrentIndex.status -ne "open" -or $Force){
             Write-Verbose "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") - Opening index $IndexName..."
-            $closeOutput = Invoke-WebRequest -Method Post -Uri "$ElasticSearchUri/$IndexName/_open" -ContentType application/json
+            $closeOutput = Invoke-WebRequest -Method Post -Uri "$ElasticSearchUri/$IndexName/_open" -ContentType application/json -UseBasicParsing
             Write-Verbose "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") - Open request complete with status: $($closeOutput.content)"
         }
         else{ Write-Warning "$IndexName is already in status open, use the force parameter if you still want to request the open index operation" }
