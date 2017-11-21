766ae7dfb6ac125dd516ef54e6da05b60097e1fc
Maekee <drmicke@gmail.com>
2017-11-21 09:50:23 +0100

    Update Get-ESIndexTemplates.ps1
    Added the UseBasicParsing parameter to Invoke-WebRequest

--- ElasticSearch/Get-ESIndexTemplates.ps1
+++ ElasticSearch/Get-ESIndexTemplates.ps1
@@ -8,7 +8,7 @@
     if($TemplateObj.Count -ne 0){
         $TemplateList = @($TemplateObj | Get-Member | Where {$_.Membertype -eq "NoteProperty"}).Name
         $TemplateList | foreach {     
-            $CurrentObj = (ConvertFrom-Json -InputObject (Invoke-WebRequest -Uri "$ElasticSearchUri/_template" -Method Get -ContentType application/json).Content).$_
+            $CurrentObj = (ConvertFrom-Json -InputObject (Invoke-WebRequest -Uri "$ElasticSearchUri/_template" -Method Get -ContentType application/json -UseBasicParsing).Content).$_
             [PSCustomObject]@{ Name = $_;Order = $CurrentObj.order;Template = $CurrentObj.template;Settings = $CurrentObj.settings;Mappings = $CurrentObj.mappings;Aliases = $CurrentObj.aliases}
         }
     }
