99cec14729ad71495b03b2cbfc54ee5916eb9352
Maekee <drmicke@gmail.com>
2019-02-15 14:53:27 +0100

    Create Get-FileEncoding.ps1

---
+++ NiceToHave/Get-FileEncoding.ps1
@@ -0,0 +1,16 @@
+# Nice little function to get encoding of files
+
+Function Get-FileEncoding($Path) {
+    $bytes = [byte[]](Get-Content $Path -Encoding byte -ReadCount 4 -TotalCount 4)
+
+    if(!$bytes) { return 'utf8' }
+
+    switch -regex ('{0:x2}{1:x2}{2:x2}{3:x2}' -f $bytes[0],$bytes[1],$bytes[2],$bytes[3]) {
+        '^efbbbf'   { return 'utf8' }
+        '^2b2f76'   { return 'utf7' }
+        '^fffe'     { return 'unicode' }
+        '^feff'     { return 'bigendianunicode' }
+        '^0000feff' { return 'utf32' }
+        default     { return 'ascii' }
+    }
+}
