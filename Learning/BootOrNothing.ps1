226407530e84207ad4f92c3399b1c05b420260a9
Maekee <drmicke@gmail.com>
2019-05-17 16:18:37 +0200

    Create BootOrNothing.ps1

---
+++ Learning/BootOrNothing.ps1
@@ -0,0 +1,18 @@
+#Sometimes when we use bool parameters and need to know if the value is false or if no value was given (not mandatory parameter).
+#We do this with $PSBoundParameters, see example below
+
+Function HelloBool{
+  param([bool]$someboolvalue)
+  
+  if($PSBoundParameters.ContainsKey('someboolvalue'){
+    if($someboolvalue){
+      Write-Host "someboolValue was given the value $true"
+    }
+    else{
+      Write-Host "someboolValue was given the value $false"
+    }
+  }
+  else{
+    Write-Host "No value was supplied"
+  }
+}
