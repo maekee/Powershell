f194bd70bb976a1cae12153f091395ac6f87c1a2
Maekee <drmicke@gmail.com>
2021-11-19 09:50:48 +0100

    Update and rename Get-HostState.ps1 to nmap-wrappers.ps1
    PowerShell nmap wrappers

--- NiceToHave/nmap-wrappers.ps1
+++ NiceToHave/nmap-wrappers.ps1
@@ -54,3 +54,48 @@ Function Get-HostState{
     elseif($output.nmaprun.runstats.hosts.down -eq 1){$false}
     else{Write-Warning -Message "Neither up or down!? $(($output.nmaprun.runstats.hosts | Select @{Name="Hosts";expression={"up $($_.up), down $($_.down), total $($_.total)"}}).Hosts)"}
 }
+
+Function Start-NetworkSweep{
+<#
+.SYNOPSIS
+    Gets up/down states on hosts in remote network (sweep)
+.DESCRIPTION
+    Uses Nmap to find all remote ip addresses in network is up or down
+.PARAMETER IPRange
+    IP network address or IP network range (e.g 192.168.100.1-254 or 192.168.100.0/24)
+.PARAMETER NmapPath
+    Optional parameter to be able to supply a nmap path if nmap is not found in PATH environment variable
+.EXAMPLE
+    Start-NetworkSweep -IPRange 192.168.100.1-100
+    Sweeps the first 100 addresses in the 192.168.100.0 network
+.NOTES
+    Script name: Start-NetworkSweep
+    Twitter:     @mickesunkan
+    Github:      https://github.com/maekee/Powershell
+#>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true,HelpMessage="Defines range e.g 192.168.100.1-254 or 192.168.100.0/24")]$IPRange,
+        [Parameter(Mandatory=$false,HelpMessage="Nmap exe path if not found in path variable")]$NmapPath
+    )
+
+    if($PSBoundParameters.ContainsKey('nmapPath')){$nmap = "`"$nmapPath`""}
+    else{$nmap = "nmap"}
+    #we could check if nmap is found by adding: Get-Command nmap -ErrorAction SilentlyContinue
+
+    [xml]$sweepoutput = Invoke-Expression -Command ". $nmap $IPRange -sn -T4 -oX -"
+
+    #-sn:   No port scan
+    #-T5:   Timing template T5 "Insane"
+    #-oX -: XML output to stdout
+
+    #-v: adds nodes with Status: down
+
+    Write-Verbose -Message "Full Nmap command executed: $($sweepoutput.nmaprun.args)"
+    Write-Verbose -Message "$($sweepoutput.nmaprun.runstats.finished.summary)"
+
+    $sweepoutput.nmaprun.host | `
+        Select  @{Name="State";expression={$_.status.state}},
+                @{Name="Address";expression={ ([xml]$_.OuterXml).Host.address.addr }},
+                @{Name="Name";expression={$_.hostnames.hostname.name}}
+}
